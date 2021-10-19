f98ef90e6b37ddccdc0a67509b3256400a0b8451
horhoj <horhoj@yandex.ru>
2021-10-19 16:04:27 +0500

    remove styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { Container } from './GlobalStyles';
 import { RoutesStructure } from './router';
 
 export const App: React.FC = () => {
   return (
-    <Container>
-      <BrowserRouter>
-        <RoutesStructure />
-      </BrowserRouter>
-    </Container>
+    <BrowserRouter>
+      <RoutesStructure />
+    </BrowserRouter>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { App } from './App';
-import { GlobalStyle } from './GlobalStyles';
 import { store } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <GlobalStyle />
       <App />
     </Provider>
   </React.StrictMode>,
